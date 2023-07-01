008dd59f2ce64d8569d3bf2e6692eda206aca28f
Replit user <>
2023-07-01 01:44:56 +0000

    changes

--- Week1/index.html/script.js
+++ Week1/index.html/script.js
@@ -1,1 +1,6 @@
+// script.js
 
+var firstName = "Linda";
+var lastName = "Eschete";
+var yearsOfStudy = 2;
+var goal = " continue to my eductaion to recieve my bachelor's after completing Year Up"; 
